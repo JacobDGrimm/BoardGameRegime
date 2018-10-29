7d9fb262bbed784c04229c6c29ab8aded2833a58
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-29 19:38:05 -0400

    adds the game table

---
+++ code/BoardGameDB/Game.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Game]
+(
+	GameID int identity not null,
+	GameTitle nvarchar(25) not null,
+	GameLength int,
+	GamePub int,
+	GameDesigner int,
+	MinNoOfPlayers int,
+	MaxNoOfPlayers int,
+	RecNoOfPlayers int,
+	GameMechinisms int,
+	GameTheme int,
+	GameComplexity int,
+	Constraint PK_Game Primary Key(GameID)
+)
