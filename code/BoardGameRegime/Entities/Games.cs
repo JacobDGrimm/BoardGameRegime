ff52d78ad9e1d148bfd875e6aa21ac98aa2d20e2
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-03 22:37:02 -0500

    adds the properties to the newly made entity

---
+++ code/BoardGameRegime/Entities/Games.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoardGameRegime.Entities
+{
+    public class Games
+    {
+        public string Title {get; set;}
+        public int GameLength { get; set; }
+        public string Publisher { get; set; }
+        public string Designer { get; set; }
+        public int MinPlayer { get; set; }
+        public int MaxPlayer { get; set; }
+        public int RecPlayer { get; set; }
+        public string Mechanism { get; set; }
+        public string Theme { get; set; }
+        public int Complexity { get; set; }
+    }
+}
