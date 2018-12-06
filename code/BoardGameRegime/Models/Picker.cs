830cc54959b248a5a46883402b87960e13619217
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-06 11:05:54 -0500

    makes the picker model but doesnt connect it yet

---
+++ code/BoardGameRegime/Models/Picker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BoardGameRegime.Models
+{
+    public class Picker
+    {
+        public int Time { get; set; }
+        public int Players { get; set; }
+        public int Complexity { get; set; }
+        public string Theme { get; set; }
+    }
+}
