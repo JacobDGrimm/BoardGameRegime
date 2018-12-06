ea107d11a851defc1c28863a7f70292c7c4c58c4
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-06 07:48:26 -0500

    adds a game controller

---
+++ code/BoardGameRegime/Controllers/GamesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using BoardGameRegime.Abstract;
+using BoardGameRegime.Entities;
+
+namespace BoardGameRegime.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        private IGameRepository repository;
+
+        public GamesController (IGameRepository gameRepository)
+        {
+            this.repository = gameRepository;
+        }
+    }
+}
