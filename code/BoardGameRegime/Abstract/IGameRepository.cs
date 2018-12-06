324c99c08806db52921eb45f4f672ad67b67809c
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-05 21:19:50 -0500

    adds the abstract folder and also adds the iGameRepository interface

---
+++ code/BoardGameRegime/Abstract/IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoardGameRegime.Entities;
+
+namespace BoardGameRegime.Abstract
+{
+    public interface IGameRepository
+    {
+        IEnumerable <Games> Game { get; }
+    }
+}
