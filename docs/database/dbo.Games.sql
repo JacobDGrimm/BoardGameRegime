6e37c81ff7b4e7328191d8d05745e92990c7600f
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-10 11:20:36 -0500

    saves the .sql file to the database docs folder

---
+++ docs/database/dbo.Games.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Games] (
+    [Title]      NVARCHAR (128) NOT NULL,
+    [GameLength] INT            NOT NULL,
+    [Publisher]  NVARCHAR (MAX) NULL,
+    [Designer]   NVARCHAR (MAX) NULL,
+    [MinPlayer]  INT            NOT NULL,
+    [MaxPlayer]  INT            NOT NULL,
+    [RecPlayer]  INT            NULL,
+    [Mechanism]  NVARCHAR (MAX) NULL,
+    [Theme]      NVARCHAR (MAX) NULL,
+    [Complexity] INT            NOT NULL,
+    CONSTRAINT [PK_dbo.Games] PRIMARY KEY CLUSTERED ([Title] ASC)
+);
+
