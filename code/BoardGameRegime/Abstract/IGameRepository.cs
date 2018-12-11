5c09bed845a7c6c1c7452fa713e2bd0e5bf10197
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-11 14:51:15 -0500

    adds slight changes to the view

--- code/BoardGameRegime/Abstract/IGameRepository.cs
+++ code/BoardGameRegime/Abstract/IGameRepository.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BoardGameRegime.Entities;
+
 
 namespace BoardGameRegime.Abstract
 {
     public interface IGameRepository
     {
-        IEnumerable <Games> Game { get; }
+        
     }
 }
